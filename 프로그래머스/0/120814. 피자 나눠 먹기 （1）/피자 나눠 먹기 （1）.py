542ba521e5fa706da47cc193bb51d0f3c2b5132e
0xxj0qxr <sbg0700@gmail.com>
2025-12-28 18:35:45 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 9.04 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    if n <= 7:
+        answer = 1
+    elif n > 7 and n % 7 > 0:
+        answer = n // 7 + 1
+    else:
+        answer = n // 7
+
+    return answer
