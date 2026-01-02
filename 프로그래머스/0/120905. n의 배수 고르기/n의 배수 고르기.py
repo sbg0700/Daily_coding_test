553c43be40caf4ab08d6f2cdd337155976a357e8
0xxj0qxr <sbg0700@gmail.com>
2026-01-02 11:39:17 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -5,10 +5,26 @@ def solution(n, numlist):
             answer.append(i)
     return answer
 
+#리스트를 요구하는 문제에서는 리스트로 답변이 가장 pythonic
+def solution(n,numlist):
+    return [i for i in numlist if i%n == 0]
 
 
-def solution(n, numlist):
-    return list(filter(lambda v: v%n==0, numlist))
+
+
+
+
+
+
+
+
+
+
+
+def solution(n,numlist):
+    return list(filter(lambda x: x%n == 0, numlist))
+
+
 
 
 
