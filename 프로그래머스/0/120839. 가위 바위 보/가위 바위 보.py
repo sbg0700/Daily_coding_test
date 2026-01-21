9e57d9bd3c72fc6f2a0c87c084490943bda2f542
0xxj0qxr <sbg0700@gmail.com>
2026-01-21 12:38:02 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,16 @@
+def solution(rsp):
+    res = ""
+
+    for i in rsp:
+        if i == "2":
+            res += "0"
+        elif i == "5":
+            res += "2"
+        else:
+            res += "5"
+
+    
+    return res
+
+    
+    return a+b+c
